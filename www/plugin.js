87b537fb939ee9582738d3687d0fd192b96ff97a
David Gallardo <David@MacBook-Pro-de-David.local>
2018-02-08 15:27:49 +0100

    plugins new method added

--- www/plugin.js
+++ www/plugin.js
@@ -4,9 +4,32 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'moverioBT350';
 
 var moverioBT350 = {
-  setCameraLedMode: function (status, successCallback, errorCallback){
-        exec(successCallback, errorCallback, PLUGIN_NAME, "setCameraLedMode", [status]);
+  setCameraLedMode: function (status, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setCameraLedMode", [status]);
+  },
+  setBacklight: function (qty, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setBacklight", [qty]);
+  },
+  setDisplayDistance: function (qty, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setDisplayDistance", [qty]);
+  },
+  setDisplayMute: function (disabled, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setDisplayMute", [disabled]);
+  },
+  setKeyEnable: function (keyLabel, enable, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setKeyEnable", [keyLabel, enable]);
+  },
+  setKeyAsisgn: function (keyLabel,keyCode, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setKeyAsisgn", [keyLabel, keyCode]);
+  },
+  setTrackpadEnable: function (enable, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, PLUGIN_NAME, "setTrackpadEnable", [enable]);
   }
+
 };
+
+
+
+
 
 module.exports = moverioBT350;
